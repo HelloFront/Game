928e4029991cd5f137358e7353affc2162ac181f
HelloFront <savchik.vl@gmail.com>
2021-10-30 18:33:19 +0300

    add balance

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const button = [...document.querySelectorAll('[data-button]')];
 const buttonContinue = document.querySelector('[data-continue]');
 const buttonExit = document.querySelector('[data-exit]');
+const buttonLose = document.querySelector('[data-lose]');
 
 let body = document.querySelector('.body');
-let win = document.querySelector('[data-win]');
-let lose = document.querySelector('[data-lose]');
+let winWindow = document.querySelector('[data-win]');
+let loseWindow = document.querySelector('[data-lose]');
+
 let outStatus = document.querySelector('.status');
 let outQuestion = document.querySelector('.question');
+let outBalanceWin = document.querySelector('.out__money__win');
+let outBalanceLose = document.querySelector('.out__money__lose');
 
 let balance = 0;
-let index = 0;
-
+let index = 5;
 
 button[0].addEventListener('click', startGame);
 // button[1].addEventListener('click', aboutGame);
@@ -31,31 +34,78 @@ function startGame () {
 function checkedAnswer () {
     let x = this.innerText;
     if (x === question[index].trueAnswer) {
-        win.style = 'display: block';
-        body.style = 'opacity: .3'
+        winWindow.style = 'display: block';
+        body.style = 'opacity: .2'
         
+        balance = countBalance();
+        outBalanceWin.innerText = `Вы выиграли ${balance}$`;
+
         index++
 
         buttonContinue.addEventListener('click', function () {
-            win.style = 'display: none';
+            winWindow.style = 'display: none';
             body.style = 'opacity: 1';
 
             startGame();
         });
+
+        buttonExit.addEventListener('click', function () {
+            winWindow.style = 'display: none';
+            body.style = 'opacity: 1';
+
+            gameOver();
+        });
+
     } else {
-        outQuestion.innerText = `Вы проиграли!`
-        outStatus.innerText = 'Ответ не правильный!';
-        setTimeout(gameOver, 800)
+        if(index >= 4 && index < 9) {
+            outBalanceLose.innerText = 'Вы выиграли 1000$';
+        } else if(index >= 9 && index < 14) {
+            outBalanceLose.innerText = 'Вы выиграли 32000$';
+        } else {
+            outBalanceLose.innerText = 'Вы ничего не выиграли';
+        }
+
+        loseWindow.style = 'display: block';
+        body.style = 'opacity: .2'
+        
+        buttonLose.addEventListener('click', function () {
+            loseWindow.style = 'display: none';
+            body.style = 'opacity: 1';
+
+            gameOver();
+        });
     }
 }
 
 function gameOver () {
     outStatus.innerText = 'Главное меню';
     outQuestion.innerText = infoGame;
-    button[0].removeEventListener('click', checkedAnswer);
+
+    for (let i = 0; i < button.length; i++) {
+        button[i].removeEventListener('click', checkedAnswer);
+    }
     button[0].addEventListener('click', startGame);
 
     for (let i = 0; i < button.length; i++) {
         button[i].innerText = arrControl[i];
     }
+
+    index = 0;
+}
+
+function countBalance () {
+
+    if(index < 3) {
+        balance += 100;
+    } else if (index === 3) {
+        balance += 200;
+    } else if (index > 3 && index < 11) {
+        balance *= 2;
+    } else if (index === 11) {
+        balance = 125000;
+    } else if (index > 11 && index <= 14){
+        balance *= 2;
+    }
+
+    return balance;
 }
