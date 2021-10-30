627703626b5bd7fe948275c7643ed0f6d9ee9cb9
HelloFront <savchik.vl@gmail.com>
2021-10-30 17:54:05 +0300

    add two window win/lose

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const button = [...document.querySelectorAll('[data-button]')];
-let index = 0;
+const buttonContinue = document.querySelector('[data-continue]');
+const buttonExit = document.querySelector('[data-exit]');
 
+let body = document.querySelector('.body');
+let win = document.querySelector('[data-win]');
+let lose = document.querySelector('[data-lose]');
 let outStatus = document.querySelector('.status');
 let outQuestion = document.querySelector('.question');
+
+let balance = 0;
+let index = 0;
+
 
 button[0].addEventListener('click', startGame);
 // button[1].addEventListener('click', aboutGame);
@@ -23,11 +31,31 @@ function startGame () {
 function checkedAnswer () {
     let x = this.innerText;
     if (x === question[index].trueAnswer) {
-        outStatus.innerText = 'Ответ правильный!';
+        win.style = 'display: block';
+        body.style = 'opacity: .3'
+        
         index++
-        setTimeout(startGame, 500)
+
+        buttonContinue.addEventListener('click', function () {
+            win.style = 'display: none';
+            body.style = 'opacity: 1';
+
+            startGame();
+        });
     } else {
+        outQuestion.innerText = `Вы проиграли!`
         outStatus.innerText = 'Ответ не правильный!';
+        setTimeout(gameOver, 800)
     }
 }
 
+function gameOver () {
+    outStatus.innerText = 'Главное меню';
+    outQuestion.innerText = infoGame;
+    button[0].removeEventListener('click', checkedAnswer);
+    button[0].addEventListener('click', startGame);
+
+    for (let i = 0; i < button.length; i++) {
+        button[i].innerText = arrControl[i];
+    }
+}
